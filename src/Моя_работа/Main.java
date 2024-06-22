fd34a2fa6552b6433904161ad9ec7fd4548c29fa
Andrei Romanov <you@example.com>
2024-06-22 22:25:45 +0300

    Reapply "Очередь у повара - добавление внеочередных"
    This reverts commit 80e5009435e743e5802427aeeb7013999d9b88d7.

--- src/Моя_работа/Main.java
+++ src/Моя_работа/Main.java
@@ -1,21 +1,29 @@
 package Моя_работа;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Zakaz> list = new LinkedList<>();
-        for (int i = 0; i < 1; i++) {
-            list.add(new Zakaz());
+        for (int i = 0; i < 5; i++) {
+            ObschiiSpisokZakazov.putZakazList(new Zakaz());
         }
-        Zakaz.put(list,new Zakaz(true));
-        Zakaz.put(list,new Zakaz(true));
-        Zakaz.put(list,new Zakaz(true));
-        list.add(new Zakaz());
-        Zakaz.put(list,new Zakaz(true));
+        SpisokVklPovarovNaRollah.addVklpovar(new Povar("Альберт"));
+        System.out.println(SpisokVklPovarovNaRollah.ktoVkl() + " Поваров работает:" + SpisokVklPovarovNaRollah.getSize());
+        System.out.println(ObschiiSpisokZakazov.getZakazList());
+        SpisokVklPovarovNaRollah.addVklpovar(new Povar("Динар"));
+        System.out.println(SpisokVklPovarovNaRollah.ktoVkl() + " Поваров работает:" + SpisokVklPovarovNaRollah.getSize());
+        System.out.println(ObschiiSpisokZakazov.getZakazList());
+        ObschiiSpisokZakazov.putZakazList(new Zakaz(true));
+        ObschiiSpisokZakazov.putZakazList(new Zakaz(true));
+        ObschiiSpisokZakazov.putZakazList(new Zakaz(true));
+        ObschiiSpisokZakazov.putZakazList(new Zakaz(true));
+        ObschiiSpisokZakazov.putZakazList(new Zakaz());
+        System.out.println(SpisokVklPovarovNaRollah.ktoVkl() + " Поваров работает:" + SpisokVklPovarovNaRollah.getSize());
+        System.out.println(ObschiiSpisokZakazov.getZakazList());
+        System.out.println("Размер листа заказов у повара:"+SpisokVklPovarovNaRollah.getVklpovar().get(0).getZakazList().size());
 
-        System.out.println(list);
 
     }
 
---
+++ src/Моя_работа/SpisokVklPovarovNaRollah.java
@@ -0,0 +1,40 @@
+package Моя_работа;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class SpisokVklPovarovNaRollah {
+    private static final List<Povar> vklpovar = new LinkedList<>();
+
+
+    public static void addVklpovar(Povar povar) {
+        if (!vklpovar.contains(povar)) {
+            //добавляем заказы в список повара из общего списка при включении повара
+            if (!ObschiiSpisokZakazov.getZakazList().isEmpty()) {
+                try {
+                    for (int i = 0; i < 2; i++) {
+                        if (ObschiiSpisokZakazov.getZakazList().get(0) != null) {
+                            povar.getZakazList().add(ObschiiSpisokZakazov.getZakazList().get(0));
+                            ObschiiSpisokZakazov.getZakazList().remove(0);
+                        } else break;
+                    }
+                } catch (IndexOutOfBoundsException e) {
+                }
+
+            }
+            vklpovar.add(povar);
+        }
+    }
+
+    public static List<Povar> getVklpovar() {
+        return vklpovar;
+    }
+
+    public static int getSize() {
+        return vklpovar.size();
+    }
+
+    public static String ktoVkl() {
+        return "" + vklpovar;
+    }
+}
